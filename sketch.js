41e8f5325838eafec36502f90f9dd7434f00dc59
AbhijitSangam <95697639+AbhijitSangam@users.noreply.github.com>
2021-12-07 20:44:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+
+function setup() {
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+  
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("BLUE"); 
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    background("RED");
+  }
+
+  
+  drawSprites();
+}
+
